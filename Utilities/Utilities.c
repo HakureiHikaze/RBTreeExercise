4d96ae53bb6dc4fe7d28d30415bdf10ab64b225e
HakureiHikaze <wgyaaa123@gmail.com>
2023-07-27 16:35:18 +0800

    Nonredundant random number generator

--- Utilities/Utilities.c
+++ Utilities/Utilities.c
@@ -16,3 +16,32 @@ uint64_t GetDJB2Hash(const char* s)
     }
     return hash;
 }
+
+NonRedundantRNG* NonRedundantRNGInit(size_t seed, int startAt, unsigned length){
+    if(length < 1) exit(-100);
+    if(length > 2147483647) exit(-101);
+    NonRedundantRNG* rng = (NonRedundantRNG*) calloc(1,sizeof (NonRedundantRNG));
+    rng->mt = InitRandMT(seed);
+    rng->list = SLListInit();
+    rng->length =length;
+    rng->startAt = startAt;
+    rng->seed = seed;
+    rng->remain = length;
+    for(int i = 0;i<(int)length;i++){
+        SLListAppend(rng->list,startAt + i);
+    }
+    return rng;
+}
+int NRRNGExtract(NonRedundantRNG* rng){
+    int temp = 0;
+    if(rng->remain) temp = SLListDelete(rng->list, ExtractRandMT(rng->mt)%rng->list->length);
+    rng->remain -=1;
+    return temp;
+}
+void NRRNGRelease(NonRedundantRNG* rng){
+    if(rng){
+        MT19937Release(rng->mt);
+        SLListRelease(rng->list);
+        free(rng);
+    }
+}
--- Utilities/Utilities.h
+++ Utilities/Utilities.h
@@ -2,7 +2,22 @@
 // Created by guoyao.wan on 2023/7/25.
 //
 #pragma once
+#include "MT19937.h"
+#include <stdint.h>
+#include<stdlib.h>
+#include "SinglyLinkedList.h"
+typedef struct nonRedundantRNG_{
+    MT19937* mt;
+    SLList* list;
+    size_t seed;
+    unsigned length;
+    int startAt;
+    unsigned remain;
+}NonRedundantRNG;
 /// DJB2 algorithm implementation
 /// \param s NULL terminated string to hash
 /// \return 64-bit hash result
 extern uint64_t GetDJB2Hash(const char* s);
+extern NonRedundantRNG* NonRedundantRNGInit(size_t seed, int startAt, unsigned length);
+extern int NRRNGExtract(NonRedundantRNG* rng);
+extern void NRRNGRelease(NonRedundantRNG* rng);
