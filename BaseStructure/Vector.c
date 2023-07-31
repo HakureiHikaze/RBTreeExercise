bb3917cbb8d0e5fc65fe4652bc31ceea68f56bb1
HakureiHikaze <wgyaaa123@gmail.com>
2023-07-31 17:31:41 +0800

    Add Vector

--- BaseStructure/Vector.c
+++ BaseStructure/Vector.c
@@ -1,2 +1,85 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include "Vector.h"
+Vector* VectorCreate(){
+    Vector* vector = (Vector*) calloc(1, sizeof(Vector ));
+    vector->data = (void**)calloc(INIT_MAX_CAPACITY, sizeof (void*));
+    vector->capacity =  INIT_MAX_CAPACITY;
+    vector->size = 0;
+    return vector;
+}
+void VectorRelease(Vector* vector){
+    if(vector){
+        if(vector->data) free(vector->data);
+        free(vector);
+    }
+}
+size_t GetLength(Vector* vector){
+    if(!vector)exit(-901);
+    return vector->size;
+}
+size_t GetCapacity(Vector* vector){
+    if(!vector)exit(-901);
+    return vector->capacity;
+}
+
+void vec_extend_(Vector * vector){
+    void** newArray;
+    if(vector->size >= 0xffffffff>>1){
+        vector->capacity = 0xffffffff;
+        newArray = (void**)calloc(0xffffffff, sizeof (void*));
+        if(!newArray) exit(-999);
+    }else{
+        newArray = (void**)calloc(vector->capacity*2, sizeof(void*));
+        if(!newArray) exit(-999);
+    }
+    vector->capacity*=2;
+    for(size_t i = 0; i< vector->size; i++){
+        newArray[i] = vector->data[i];
+    }
+    vector->data = newArray;
+}
+
+void VectorAppend(Vector* vector, void* data){
+    if(!vector)exit(-901);
+    if(vector->size >= vector->capacity-1){
+        vec_extend_(vector);
+    }
+    vector->data[vector->size] = data;
+    vector->size+=1;
+}
+
+void VectorInsertAt(Vector* vector, void* data, size_t index){
+    if(!vector)exit(-901);
+    if(index> vector->size) exit(-501);
+    if(vector->size >= vector->capacity-1){
+        vec_extend_(vector);
+    }
+    for(size_t i = index; i<vector->size; i++){
+        vector->data[i+1] = vector->data[i];
+    }
+    vector->data[index] = data;
+}
+
+void* VectorDelete(Vector* vector,size_t index){
+    if(!vector)exit(-901);
+    if(index>= vector->size) exit(-501);
+    void* temp = vector->data[index];
+    for(size_t i = index; i<vector->size-1;i++){
+        vector->data[i] = vector->data[i+1];
+    }
+    vector->data[vector->size-1] = 0;
+    vector->size-=1;
+    return temp;
+}
+void* VectorAtIndex(Vector* vector,size_t index){
+    if(!vector)exit(-901);
+    if(index>= vector->size) exit(-501);
+    return vector->data[index];
+}
+size_t VectorFindFirst(Vector* vector,void* data){
+    if(!vector)exit(-901);
+    for(size_t i = 0; i< vector->size;i++){
+        if(vector->data[i] == data)return i;
+    }
+    exit(-502);
+}
--- BaseStructure/Vector.h
+++ BaseStructure/Vector.h
@@ -1,10 +1,17 @@
 #pragma once
+#define INIT_MAX_CAPACITY 16
 typedef struct Vector_{
     size_t capacity;
     size_t size;
     void** data;
 }Vector;
+
+extern Vector* VectorCreate();
+extern void VectorRelease(Vector*);
+extern size_t GetLength(Vector*);
+extern size_t GetCapacity(Vector*);
 extern void VectorAppend(Vector*, void*);
+extern void VectorInsertAt(Vector*, void*, size_t);
 extern void* VectorDelete(Vector*,size_t);
 extern void* VectorAtIndex(Vector*,size_t);
 extern size_t VectorFindFirst(Vector*,void*);
